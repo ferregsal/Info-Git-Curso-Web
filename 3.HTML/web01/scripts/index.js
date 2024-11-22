4a420aeccf0b5004fe8501daf67d3ff9cbbffa6c
Alejandro Cerezo <alce65@hotmail.es>
2024-11-22 11:47:38 +0100

    Update samples for last html tags

--- 3.HTML/web01/scripts/index.js
+++ 3.HTML/web01/scripts/index.js
@@ -1,1 +1,12 @@
 console.log('Loaded index');
+
+function handleSubmit(ev) {
+    ev.preventDefault();
+    console.log(ev);
+    const elements = ev.target.elements;
+    console.log(elements.namedItem('firstname').value);
+}
+
+const form = document.querySelector('form');
+console.dir(form);
+//form.addEventListener('submit', handleSubmit);
