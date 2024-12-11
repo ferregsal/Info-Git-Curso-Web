af0275e8644556ca64a2eae6067eb4016b780f3c
ferregsal <fregidorsalazar@gmail.com>
2024-12-11 10:48:29 +0100

    Add Changes

--- 5.JS/samples/main.js
+++ 5.JS/samples/main.js
@@ -1,6 +1,23 @@
-import { countWords } from './tools.js';
+import {
+    countWords,
+    randomLetter,
+    randomDigit,
+    generatePassword,
+    shuffleString,
+} from './tools.js';
 
 console.log('Soy main');
 
 const sentence = 'Hola';
 console.log(countWords(sentence));
+
+// for (let i = 0; i < 10; i++) {
+//     console.log('Random Letter' + ' ' + randomLetter());
+// }
+// for (let i = 0; i < 10; i++) {
+//     console.log('Random number' + ' ' + randomDigit());
+// }
+// for (let i = 0; i < 10; i++) {
+//     console.log('Password' + ' ' + generatePassword(i + 1));
+// }
+console.log(generatePassword(13));
--- 5.JS/samples/tools.js
+++ 5.JS/samples/tools.js
@@ -18,3 +18,47 @@ export function countWords(sentence = '') {
 // letras mayúsculas,
 // letras minúsculas
 // y números.
+export function randomDigit() {
+    return Math.trunc(Math.random() * 10);
+}
+
+export function randomIntegerInInterval(min, max) {
+    return Math.trunc(Math.random() * (max - min) + min);
+}
+export function randomLetter() {
+    const letters = 'abcdefghijklmnñopqrstuvwxyz';
+    const position = [randomIntegerInInterval(0, letters.length - 1)];
+    return letters[position];
+}
+
+export function shuffleString(word = '') {
+    const initialLetters = word.split('');
+    const finalLetters = [];
+
+    for (let i = 0; i < word.length; i++) {
+        const n = randomIntegerInInterval(0, initialLetters.length - 1);
+        const letter = initialLetters.splice(n, 1);
+        finalLetters.push(letter);
+    }
+
+    return finalLetters.join('');
+}
+
+export function generatePassword(Lenght) {
+    let rounds = 2;
+    if (Lenght > 6) {
+        rounds = Math.ceil(Lenght / 3);
+    }
+    let result = '';
+    for (let i = 0; i < rounds; i++) {
+        result += randomIntegerInInterval(0, 9);
+        result += randomLetter();
+        result += randomLetter().toUpperCase();
+    }
+    // if (Lenght > 6) result = result.slice(0, Lenght);
+
+    //  return result;
+
+    result = Lenght <= 6 ? result : (result = result.slice(0, Lenght));
+    return shuffleString(result);
+}
