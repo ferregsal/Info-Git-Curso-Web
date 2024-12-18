a80ca5f90a6d383e63f983bb23a852e734209c6e
Alejandro Cerezo <alce65@hotmail.es>
2024-12-18 22:13:10 +0100

    Add toggle button for change theme

--- 5.JS/router/porfolio/porfolio.js
+++ 5.JS/router/porfolio/porfolio.js
@@ -4,4 +4,14 @@ export function loadPorfolio() {
     console.log('loaded porfolio');
     const pageTitle = 'Porfolio';
     createHeader(pageTitle);
+
+    const doc = document.documentElement;
+
+    document
+        .querySelector('.toggle-switch input')
+        .addEventListener('change', (event) => {
+            const isDark = event.target.checked;
+            console.log('toggle-switch', { isDark });
+            doc.dataset.theme = isDark ? 'dark' : 'light';
+        });
 }
