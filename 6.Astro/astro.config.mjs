16e59e40666ee2a65615e84d43fba401ff0d5efe
Alejandro Cerezo <alce65@hotmail.es>
2025-02-10 17:58:28 +0100

    Add Node Adapter to Astro project

--- 6.Astro/astro.config.mjs
+++ 6.Astro/astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
     experimental: {
         svg: true,
     },
+
+    adapter: node({
+        mode: 'middleware',
+    }),
 });
