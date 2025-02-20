87419b65d95fb49cad910967d3e58bb046f2880c
Alejandro Cerezo <alce65@hotmail.es>
2025-02-20 21:14:10 +0100

    Update movies project

--- 13.DB/src/entities.ts
+++ 13.DB/src/entities.ts
@@ -1,5 +1,12 @@
 import type { RowDataPacket } from 'mysql2/promise';
 
+export type SqlError = {
+    code: string;
+    errno: number;
+    sqlState: string;
+    sqlMessage: string;
+} & Error;
+
 // interface Category extends mysql.RowDataPacket {
 //     id: number;
 //     name: string;
@@ -9,3 +16,15 @@ export type Generes = {
     id: number;
     name: string;
 } & RowDataPacket;
+
+export type Movie = {
+    id: string;
+    title: string;
+    year: number;
+    duration: number;
+    director: string;
+    poster: string;
+    rate: number;
+};
+
+export type MovieRow = Movie & RowDataPacket;
