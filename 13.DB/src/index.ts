2aa38958ba91fc81c2461f5e2fc236d8f698bd2c
Alejandro Cerezo <alce65@hotmail.es>
2025-02-20 13:42:20 +0100

    Add generes CRUD with mysql2 in Node

--- 13.DB/src/index.ts
+++ 13.DB/src/index.ts
@@ -1,19 +1,23 @@
-import { loadEnvFile } from 'node:process';
-import mysql from 'mysql2/promise';
+// import { loadEnvFile, } from 'node:process';
 
-loadEnvFile('.env');
-
-const dataConnection = {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || '',
-};
+import { openConnection } from './db.js';
+import { ManageGeneres } from './generes.js';
+process.loadEnvFile('.env');
 
 try {
-    const connection = await mysql.createConnection(dataConnection);
-    console.log('Connection', connection);
+    const connection = await openConnection();
+    const manageGeneres = new ManageGeneres(connection);
+    const generes = await manageGeneres.getAllGeneres();
+    console.log(generes);
+    // const result = await createGenere('War');
+    // console.log(result);
+    // const result2 = await updateGenere(25, 'Drama');
+    // console.log(result2);
+    // for (let i = 14; i < 29; i++) {
+    //     const result = await deleteGenere(i);
+    //     console.log(result);
+    // }
+    // connection.end();
 } catch (error) {
     if (error instanceof Error) {
         console.error(error);
