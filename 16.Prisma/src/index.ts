794a347aea76ffc031607fec6323899aa42c940c
Alejandro Cerezo <alce65@hotmail.es>
2025-02-28 13:06:48 +0100

    Add new project for Prisma exercises

--- 16.Prisma/src/index.ts
+++ 16.Prisma/src/index.ts
@@ -1,1 +1,30 @@
+import { $Enums, PrismaClient, Prisma } from '@prisma/client';
+
 console.log('Hello, world!');
+const prisma = new PrismaClient();
+
+export const getCountries = async () => {
+    const countries = await prisma.country.findMany();
+    console.log(countries);
+};
+
+export const getCountriesByContinents = async (
+    continent: $Enums.CountryContinent,
+) => {
+    const selectOptions: Prisma.CountrySelect = {
+        code: true,
+        name: true,
+        capital: true,
+    };
+
+    const countries = await prisma.country.findMany({
+        where: {
+            continent: continent,
+        },
+        select: selectOptions,
+    });
+    console.log(countries);
+};
+
+// await getCountries();
+// await getCountriesByContinents('Europe');
