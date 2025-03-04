1dd5ad0c23d0eec4802cf2026f5022014e37a441
Alejandro Cerezo <alce65@hotmail.es>
2025-03-04 14:01:54 +0100

    Add DTOs with Zod validation

--- 15.Films/src/controllers/users.controller.ts
+++ 15.Films/src/controllers/users.controller.ts
@@ -4,10 +4,12 @@ import type { AppResponse } from '../types/app-response';
 import { UsersRepo, UserWithoutPasswd } from '../repo/users.repository.js';
 import { AuthService } from '../services/auth.service.js';
 import { HttpError } from '../types/http-error.js';
+import { UserCreateDTO, UserLoginDTO } from '../dto/users.dto.js';
+import { ZodError } from 'zod';
 const debug = createDebug('films:controllers:users');
 
 export class UsersController {
-    constructor(private repoFilms: UsersRepo) {
+    constructor(private repoUsers: UsersRepo) {
         debug('Instanciando');
     }
 
@@ -19,17 +21,18 @@ export class UsersController {
         return data;
     }
 
-    create = async (req: Request, res: Response, next: NextFunction) => {
+    async create(req: Request, res: Response, next: NextFunction) {
         debug('create');
         try {
             const newData = req.body;
+            UserCreateDTO.parse(newData);
             newData.password = await AuthService.hashPassword(newData.password);
-            const user = await this.repoFilms.create(newData);
+            const user = await this.repoUsers.create(newData);
             res.json(this.makeResponse([user]));
         } catch (error) {
             next(error);
         }
-    };
+    }
 
     async login(req: Request, res: Response, next: NextFunction) {
         const error = new HttpError(
@@ -41,7 +44,18 @@ export class UsersController {
         try {
             const { email, password: clientPassword } = req.body;
 
-            const user = await this.repoFilms.getByEmail(email);
+            // if (!email || !clientPassword) {
+            //     throw error;
+            // }
+
+            try {
+                UserLoginDTO.parse({ email, password: clientPassword });
+            } catch (err) {
+                error.message = (err as ZodError).message; //.errors[0].message;
+                throw error;
+            }
+
+            const user = await this.repoUsers.getByEmail(email);
             if (user === null) {
                 throw error;
             }
@@ -49,6 +63,7 @@ export class UsersController {
             // user.password; // base de datos -> encriptado
 
             const { password: hashedPassword, ...userWithoutPasswd } = user;
+
             const isValid = await AuthService.comparePassword(
                 clientPassword,
                 hashedPassword,
@@ -56,7 +71,19 @@ export class UsersController {
             if (!isValid) {
                 throw error;
             }
-            res.json(this.makeResponse([userWithoutPasswd]));
+
+            const token = await AuthService.generateToken({
+                id: userWithoutPasswd.id,
+                email: userWithoutPasswd.email,
+            });
+
+            const response = {
+                ...userWithoutPasswd,
+                token,
+            };
+
+            res.cookie('token', token);
+            res.json(this.makeResponse([response]));
         } catch (error) {
             next(error);
         }
