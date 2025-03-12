93ed5bcfc3edc1691c055f5f0c421575b9659c86
Alejandro Cerezo <alce65@hotmail.es>
2025-03-12 14:03:09 +0100

    Add register and login components

---
+++ 17.ng/projects/demo/src/app/user/login/login.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cas-login',
+  imports: [],
+  template: ` <p>aquí irá el formulario de login</p> `,
+  styles: ``,
+})
+export class LoginComponent {}
---
+++ 17.ng/projects/demo/src/app/user/register/register.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cas-register',
+  imports: [],
+  template: ` <p>aquí irá el formulario de register</p> `,
+  styles: ``,
+})
+export class RegisterComponent {}
