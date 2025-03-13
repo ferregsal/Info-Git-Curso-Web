49a30ee32e7c1698f58897ec6907b1cadff83062
Alejandro Cerezo <alce65@hotmail.es>
2025-03-13 14:03:12 +0100

    Add counter with signals and use output() in counters

--- 17.ng/projects/demo/src/app/about/about.component.ts
+++ 17.ng/projects/demo/src/app/about/about.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CounterComponent } from './counter/counter.component';
 import { GreetingComponent } from './greeting/greeting.component';
+import { CounterSignalComponent } from './counter.signal/counter.signal.component';
 
 @Component({
   selector: 'cas-about',
-  imports: [CounterComponent, GreetingComponent],
+  imports: [CounterComponent, GreetingComponent, CounterSignalComponent],
   template: `
     <h2>About</h2>
     <p>Esta aplicación es una demo del uso de Angular</p>
     <p>A continuación, un ejemplo de componente contador</p>
-    <cas-counter></cas-counter>
+    <cas-counter (countEvent)="setCounter()"></cas-counter>
+    <p>A continuación, un ejemplo de componente contador con Signals</p>
+    <cas-counter-signal (countEvent)="setCounter()"></cas-counter-signal>
+    <p>Total de clicks: {{ clicks() }}</p>
     <p>A continuación, un ejemplo de un componente que saluda al usuario</p>
     <cas-greeting></cas-greeting>
   `,
   styles: ``,
 })
-export default class AboutComponent {}
+export default class AboutComponent {
+  clicks = signal(0);
+
+  setCounter() {
+    this.clicks.update((prev) => prev + 1);
+  }
+}
---
+++ 17.ng/projects/demo/src/app/about/counter.signal/counter.signal.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CounterSignalComponent } from './counter.signal.component';
+
+describe('CounterComponent', () => {
+  let component: CounterSignalComponent;
+  let fixture: ComponentFixture<CounterSignalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CounterSignalComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CounterSignalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
