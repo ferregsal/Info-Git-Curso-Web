76611728929b6b33b90c3856bf76e548aae48292
Alejandro Cerezo <alce65@hotmail.es>
2025-03-14 14:01:10 +0100

    Add service sample: TimeService

--- 17.ng/projects/demo/src/app/home/home.component.ts
+++ 17.ng/projects/demo/src/app/home/home.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RegisterComponent } from '../user/register/register.component';
 import { LoginComponent } from '../user/login/login.component';
+import { TimeService } from '../core/services/time.service';
 
 @Component({
   selector: 'cas-home',
   imports: [RegisterComponent, LoginComponent],
+  providers: [TimeService],
   template: `
     <h1>Home</h1>
     <p>Esta es la página de inicio</p>
     <cas-register></cas-register>
     <cas-login></cas-login>
+    {{ timeService.getTime() }}
   `,
   styles: ``,
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+  timeService = inject(TimeService);
+}
