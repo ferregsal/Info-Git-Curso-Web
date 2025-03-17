0d040e7a90ba1dc56d552ad554988bc6719df896
Alejandro Cerezo <alce65@hotmail.es>
2025-03-17 11:00:40 +0100

    Change film route protection

--- 15.Films/src/router/films.router.ts
+++ 15.Films/src/router/films.router.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { FilmsController } from '../controllers/films.controller.js';
 import createDebug from 'debug';
 import { AuthInterceptor } from '../middleware/auth.interceptor.js';
-import { Role } from '@prisma/client';
+// import { Role } from '@prisma/client';
 
 const debug = createDebug('movies:router:films');
 
@@ -17,26 +17,26 @@ export const createFilmsRouter = (
     filmsRouter.get('/:id', filmsController.getById);
     filmsRouter.post(
         '/',
-        authInterceptor.authenticate,
-        authInterceptor.hasRole(Role.EDITOR),
+        // authInterceptor.authenticate,
+        // authInterceptor.hasRole(Role.EDITOR),
         filmsController.create,
     );
     filmsRouter.patch(
         '/:id',
-        authInterceptor.authenticate,
-        authInterceptor.hasRole(Role.EDITOR),
+        // authInterceptor.authenticate,
+        // authInterceptor.hasRole(Role.EDITOR),
         filmsController.update,
     );
     filmsRouter.patch(
         '/:id/category/:name',
-        authInterceptor.authenticate,
-        authInterceptor.hasRole(Role.EDITOR),
+        // authInterceptor.authenticate,
+        // authInterceptor.hasRole(Role.EDITOR),
         filmsController.toggleCategory,
     );
     filmsRouter.delete(
         '/:id',
-        authInterceptor.authenticate,
-        authInterceptor.hasRole(Role.EDITOR),
+        // authInterceptor.authenticate,
+        // authInterceptor.hasRole(Role.EDITOR),
         filmsController.delete,
     );
     return filmsRouter;
